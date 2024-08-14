998c650d613985962f6e080568a6ff69c80da028
Claire Roughan <claire.roughan@theculturetrip.com>
2024-08-14 16:50:47 +0100

    Project setup

---
+++ HuliPizzaSwiftUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  HuliPizzaSwiftUI
+//
+//  Created by Claire Roughan on 14/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("Hello world!")
+    }
+}
+
+#Preview {
+    ContentView()
+    
+}
---
+++ HuliPizzaSwiftUI/HuliPizzaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaSwiftUIApp.swift
+//  HuliPizzaSwiftUI
+//
+//  Created by Claire Roughan on 14/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
