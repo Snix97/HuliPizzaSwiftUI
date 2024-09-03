34b17ec48112a8b6ee07707b697385073b4948a2
Claire Roughan <ClaireRoughan.Dev@gmail.com>
2024-09-03 14:26:47 +0100

    Tidyup RatingsView code

--- HuliPizzaSwiftUI/Ratings/RatingsView.swift
+++ HuliPizzaSwiftUI/Ratings/RatingsView.swift
@@ -5,4 +5,23 @@
 //  Created by Claire Roughan on 03/09/2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct RatingsView: View {
+    var rating:Int
+    
+    var body: some View {
+        HStack{
+            ForEach(1...6, id:\.self) { circle in
+                Image(systemName: (circle <= rating) ? "fork.knife.circle.fill": "circle")
+            }
+        }
+    }
+}
+
+
+struct RatingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        RatingsView(rating: 5)
+    }
+}
